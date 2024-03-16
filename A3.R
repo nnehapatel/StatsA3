7c6c6b6f8e88891e79444519dc082d6dbda20b18
Neha <neha_pat@outlook.com>
2024-03-16 14:57:27 -0400

    Create model matrix, data set splitting, and make search grid of alpha values for use with elastic net

--- A3.R
+++ A3.R
@@ -16,3 +16,29 @@ head(immuno)
 sapply(immuno, class)
 sapply(immuno, function(x) sum(is.na(x)))
 
+# convert severity and sex into numeric values
+immuno$Severirty <- as.numeric(immuno$Severirty == "Severe")
+immuno$SEX <- as.numeric(immuno$SEX == "M")
+
+# fit a linear model with severity as the response variable
+# exclude patient number, which is not a predictor variable
+f0 <- lm(Severirty ~., data=immuno[,-1])
+
+# view model and model matrix
+summary(f0)
+str(model.matrix(f0))
+head(model.matrix(f0))
+
+# remove intercept from model matrix
+X <- model.matrix(f0)[,-1]
+
+# set y as the response variable
+y <- immuno$Severirty
+table(y)
+
+# set the indexes of 20% of the dataset for the test set
+set.seed(1717)
+test.index <- sample.int(dim(X)[1],round(dim(X)[1]*.2), replace = FALSE)
+
+# make a search grid for alpha
+alpha_grid <- seq(0,1,.01)
